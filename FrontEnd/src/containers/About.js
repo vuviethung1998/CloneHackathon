4bf07d0cb551659cadb7901f1f216b4bdefc4896
vuviethung1998 <vuviethung.98.hust@gmail.com>
2019-01-28 18:30:31 +0700

    update

--- FrontEnd/src/containers/About.js
+++ FrontEnd/src/containers/About.js
@@ -6,6 +6,13 @@ export default class About extends Component {
     return (
       <div>
         <Bar/>
+        <h1  className="text-center">Hello everyone</h1>
+        <h2  className="text-center">Day la team GGWP</h2>
+        <h2  className="text-center">Team chung toi gom 3 nguoi:</h2>
+        <h3  className="text-center">Vu Viet Hung</h3>
+        <h3  className="text-center">Nguyen Thanh Nam</h3>
+        <h3  className="text-center">Dinh</h3>
+        <br />  
       </div>
     )
   }
--- FrontEnd/src/containers/Login.js
+++ FrontEnd/src/containers/Login.js
@@ -1,55 +1,73 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from '../axios';
 import Bar from '../components/Bar';
 import config from '../config'
 
 export default class login extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      username: "",
-      password: ""
+    constructor(props) {
+        super(props)
+        this.state = {
+            username: "",
+            password: ""
+        }
+        this.handleChange = this
+            .handleChange
+            .bind(this);
+        this.handleSubmit = this
+            .handleSubmit
+            .bind(this);
     }
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
 
-  handleChange(event) {
-    this.setState({[event.target.name]: event.target.value});
-  }
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
 
-  handleSubmit = (event) => {
-    alert('connecting username: ' + this.state.username);
-    event.preventDefault();
-    axios
-      .post( config.rootPath +"/admin/login" ,{
-        username : this.state.username,
-        password : this.state.password
-      })
-      .then(data => {
-        window.location.href="/admin";
-      })
-      .catch((err) => {
-        console.error(err);
-    });
-  }
+    handleSubmit = (event) => {
+        alert('connecting username: ' + this.state.username);
+        event.preventDefault();
+        axios
+            .post(config.rootPath + "/admin/login", {
+            username: this.state.username,
+            password: this.state.password
+        })
+            .then(data => {
+                window.location.href = "/admin";
+            })
+            .catch((err) => {
+                console.error(err);
+            });
+    }
 
-  render() {
-    return (
-      <div>
-        <Bar/>
-        
-        <form onSubmit = {this.handleSubmit}>
-          <label>
-            <input type="text" name="username" onChange={this.handleChange}/>
-            <input type="password" name="password" onChange={this.handleChange}/>
-          </label>
-          <div>
-              <input type="submit" value="submit"/>
-          </div>
-          
-        </form>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Bar/>
+                <h1
+                    className="text-center"
+                    style={{
+                    margin: "50px"
+                }}>Login page</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="text-center">
+                        <div>
+                            <label>Username:
+                                <input type="text" name="username" onChange={this.handleChange}/>
+                            </label>
+                        </div>
+                        <div>
+                            <label>Password: 
+                                <input type="password" name="password" onChange={this.handleChange}/>
+                            </label>
+                        </div>
+                        <div>
+                            <input type="submit" value="SUBMIT"/>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        )
+    }
 }
